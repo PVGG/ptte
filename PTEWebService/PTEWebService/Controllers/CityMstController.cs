5c11faf4b5345a299d43b9d6756aef9aec3bda46
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-19 16:41:30 +0530

    Create New Apis

---
+++ PTEWebService/PTEWebService/Controllers/CityMstController.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PTEWebService.Controllers
+{
+    public class CityMstController : ApiController
+    {
+        // POST: api/citymst/5
+        public string Get(int id)
+        {
+            Models.City_Mst obj = new Models.City_Mst();
+
+            string output = obj.GetCity(id);
+
+            return output;
+
+        }
+
+        // POST: api/citymst
+        public string Get()
+        {
+            Models.City_Mst obj = new Models.City_Mst();
+
+            string output = obj.GetAllCity();
+
+            return output;
+        }
+
+        // POST: api/citymst
+        //public string GetValss(int id,string value)
+        //public string Post([FromBody]string value)
+        public string Post([FromBody]string value)
+        {
+            Models.City_Mst obj = new Models.City_Mst();
+
+            dynamic jsonArrs = JArray.Parse(value);
+
+            dynamic jsonArr = jsonArrs[0];
+
+            int country_id = jsonArr.country_id;
+            int zone_id = jsonArr.zone_id;
+            int state_id = jsonArr.state_id;
+            string city_name = jsonArr.city_name;
+            int user_id = jsonArr.user_id;
+            //JSONArray ja_data = json.getJSONArray("data");
+
+            string output = obj.AddCity(country_id,zone_id, state_id,city_name, user_id);
+            //return id.ToString();
+            return output;
+            //return value;
+        }
+
+        // PUT: api/citymst/5
+        public string Put(int id, [FromBody]string value)
+        {
+            Models.City_Mst obj = new Models.City_Mst();
+
+            dynamic jsonArrs = JArray.Parse(value);
+
+            dynamic jsonArr = jsonArrs[0];
+
+            int country_id = jsonArr.country_id;
+            int zone_id = jsonArr.zone_id;
+            int state_id = jsonArr.state_id;
+            string city_name = jsonArr.city_name;
+            int user_id = jsonArr.user_id;
+            //int index_id = jsonArr.index_id;
+            //JSONArray ja_data = json.getJSONArray("data");
+
+            string output = obj.EditCity(id, country_id, zone_id, state_id, city_name, user_id);
+
+            return output;
+        }
+
+        // DELETE: api/citymst/5
+        public string Delete(int id)
+        {
+            Models.City_Mst obj = new Models.City_Mst();
+
+            string output = obj.DeleteCity(id);
+
+            return output;
+        }
+    }
+}
---
+++ PTEWebService/PTEWebService/Controllers/CountryMstController.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PTEWebService.Controllers
+{
+    public class CountryMstController : ApiController
+    {
+        // POST: api/countrymst/5
+        public string Get(int id)
+        {
+            Models.Country_Mst obj = new Models.Country_Mst();
+
+            string output = obj.GetCountry(id);
+
+            return output;
+
+        }
+
+        // POST: api/countrymst
+        public string Get()
+        {
+            Models.Country_Mst obj = new Models.Country_Mst();
+
+            string output = obj.GetAllCountry();
+
+            return output;
+        }
+
+        // POST: api/countrymst
+        //public string GetValss(int id,string value)
+        //public string Post([FromBody]string value)
+        public string Post([FromBody]string value)
+        {
+            Models.Country_Mst obj = new Models.Country_Mst();
+
+            dynamic jsonArrs = JArray.Parse(value);
+
+            dynamic jsonArr = jsonArrs[0];
+
+            string country_name = jsonArr.country_name;
+            //JSONArray ja_data = json.getJSONArray("data");
+
+            string output = obj.AddCountry(country_name);
+            //return id.ToString();
+            return output;
+            //return value;
+        }
+
+        // PUT: api/countrymst/5
+        public string Put(int id, [FromBody]string value)
+        {
+            Models.Country_Mst obj = new Models.Country_Mst();
+
+            dynamic jsonArrs = JArray.Parse(value);
+
+            dynamic jsonArr = jsonArrs[0];
+
+            string country_name = jsonArr.country_name;
+            //int index_id = jsonArr.index_id;
+            //JSONArray ja_data = json.getJSONArray("data");
+
+            string output = obj.EditCountry(id, country_name);
+
+            return output;
+        }
+
+        // DELETE: api/countrymst/5
+        public string Delete(int id)
+        {
+            Models.Country_Mst obj = new Models.Country_Mst();
+
+            string output = obj.DeleteCountry(id);
+
+            return output;
+        }
+    }
+}
