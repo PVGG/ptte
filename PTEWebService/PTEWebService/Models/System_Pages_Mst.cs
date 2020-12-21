5655b0d6d0a242498dc71b38ebfed944ce246f34
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-21 17:56:07 +0530

    Create New Api

---
+++ PTEWebService/PTEWebService/Models/System_Pages_Mst.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace PTEWebService.Models
+{
+    public class System_Pages_Mst
+    {
+        public int index_id { get; set; }
+        public string pages_name { get; set; }
+        public string pages_url { get; set; }
+
+        public int user_id { get; set; }
+
+        private SqlConnection con;
+        private SqlCommand com;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+        }
+        #region Get All System Pages
+        public string GetAllSystemPages()
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "SelectAll");
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Get System Pages from Index ID
+        public string GetSystemPages(int id)
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "Select");
+                com.Parameters.AddWithValue("@index_id", user_id);
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Insert System Pages
+        public string AddSystemPages(string pages_name, string pages_url, int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Insert");
+            com.Parameters.AddWithValue("@module_name", area_name);
+            com.Parameters.AddWithValue("@created_by", created_by_id);
+            com.Parameters.AddWithValue("@is_deleted", false);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+            {
+                return "Data Added Successfully";
+            }
+            else
+            {
+                return "Data Not Added";
+            }
+        }
+        #endregion
+        #region Update System Pages
+        public string EditSystemPages(int index_id, string pages_name, string pages_url, int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Update");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@module_name", area_name);
+            com.Parameters.AddWithValue("@updated_date", DateTime.Now.ToShortDateString());
+            com.Parameters.AddWithValue("@updated_by", "UserId");
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Updated Successfully";
+            }
+            else
+            {
+                return "Data Not Updated";
+            }
+        }
+        #endregion
+        #region Delete System Pages
+        public string DeleteSystemPages(int index_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Delete");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@is_deleted", true);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Deleted Successfully";
+            }
+            else
+            {
+                return "Data Not Deleted";
+            }
+        }
+        #endregion
+    }
+}
