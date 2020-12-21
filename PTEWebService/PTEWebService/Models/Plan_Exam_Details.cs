630ce1220cfb14c3677f68e509d4e15bd7e91b5e
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-21 13:46:49 +0530

    Create New Api

---
+++ PTEWebService/PTEWebService/Models/Plan_Exam_Details.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace PTEWebService.Models
+{
+    public class Plan_Exam_Details
+    {
+        public int index_id { get; set; }
+        public int plan_id { get; set; }
+        public int plan_detail_id { get; set; }
+        public int exam_id { get; set; }
+        public int user_id { get; set; }
+
+        
+
+
+        private SqlConnection con;
+        private SqlCommand com;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+        }
+        #region Get All Plan Exam Details
+        public string GetAllPlanExamDetails()
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "SelectAll");
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Get Plan Exam Details from Index ID
+        public string GetPlanExamDetails(int id)
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "Select");
+                com.Parameters.AddWithValue("@index_id", user_id);
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Insert Plan Exam Details
+        public string AddPlanExamDetails(int plan_id, int plan_detail_id, int exam_id, int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Insert");
+            com.Parameters.AddWithValue("@module_name", notes);
+            com.Parameters.AddWithValue("@created_by", user_id);
+            com.Parameters.AddWithValue("@is_deleted", false);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+            {
+                return "Data Added Successfully";
+            }
+            else
+            {
+                return "Data Not Added";
+            }
+        }
+        #endregion
+        #region Update Plan Exam Details
+        public string EditPlanExamDetails(int index_id, int plan_id, int plan_detail_id, int exam_id, int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Update");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@module_name", notes);
+            com.Parameters.AddWithValue("@updated_date", DateTime.Now.ToShortDateString());
+            com.Parameters.AddWithValue("@updated_by", "UserId");
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Updated Successfully";
+            }
+            else
+            {
+                return "Data Not Updated";
+            }
+        }
+        #endregion
+        #region Delete Plan Exam Details
+        public string DeletePlanExamDetails(int index_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Delete");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@is_deleted", true);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Deleted Successfully";
+            }
+            else
+            {
+                return "Data Not Deleted";
+            }
+        }
+        #endregion
+    }
+}
