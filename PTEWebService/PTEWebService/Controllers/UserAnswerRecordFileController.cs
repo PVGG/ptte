e3a324acf874b30b7e26e810d0ff470df01b031b
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-21 11:07:05 +0530

    Create New Api

--- PTEWebService/PTEWebService/Controllers/UserAnswerRecordFileController.cs
+++ PTEWebService/PTEWebService/Controllers/UserAnswerRecordFileController.cs
@@ -8,36 +8,36 @@ using System.Web.Http;
 
 namespace PTEWebService.Controllers
 {
-    public class UserAnswerController : ApiController
+    public class UserAnswerRecordFileController : ApiController
     {
-        // POST: api/useranswer/5
+        // POST: api/useranswerrecordfile/5
         public string Get(int id)
         {
 
-            Models.User_Answer obj = new Models.User_Answer();
+            Models.User_Answer_Record_File obj = new Models.User_Answer_Record_File();
 
-            string output = obj.GetUserAnswer(id);
+            string output = obj.GetUserAnswerRecordFile(id);
 
             return output;
 
         }
 
-        // POST: api/useranswer
+        // POST: api/useranswerrecordfile
         public string Get()
         {
-            Models.User_Answer obj = new Models.User_Answer();
+            Models.User_Answer_Record_File obj = new Models.User_Answer_Record_File();
 
-            string output = obj.GetAllUserAnswer();
+            string output = obj.GetAllUserAnswerRecordFile();
 
             return output;
         }
 
-        // POST: api/useranswer
+        // POST: api/useranswerrecordfile
         //public string GetValss(int id,string value)
         //public string Post([FromBody]string value)
         public string Post([FromBody]string value)
         {
-            Models.User_Answer obj = new Models.User_Answer();
+            Models.User_Answer_Record_File obj = new Models.User_Answer_Record_File();
 
             dynamic jsonArrs = JArray.Parse(value);
 
@@ -46,22 +46,22 @@ namespace PTEWebService.Controllers
             int exam_id = jsonArr.exam_id;
             int question_id = jsonArr.question_id;
             int user_id = jsonArr.user_id;
-            string answer = jsonArr.answer;
-            int total_record_time = jsonArr.total_record_time;
-            string answer_submitted_date_time = jsonArr.answer_submitted_date_time;
+            string av_file_url = jsonArr.av_file_url;
+            int duration_of_file = jsonArr.duration_of_file;
+            string recorded_date_time = jsonArr.recorded_date_time;
             int created_by_id = jsonArr.created_by_id;
             //JSONArray ja_data = json.getJSONArray("data");
 
-            string output = obj.AddUserAnswer(exam_id,question_id,user_id,answer,total_record_time,answer_submitted_date_time,created_by_id);
+            string output = obj.AddUserAnswerRecordFile(exam_id, question_id, user_id, av_file_url, duration_of_file, recorded_date_time, created_by_id);
             //return id.ToString();
             return output;
             //return value;
         }
 
-        // PUT: api/useranswer/5
+        // PUT: api/useranswerrecordfile/5
         public string Put(int id, [FromBody]string value)
         {
-            Models.User_Answer obj = new Models.User_Answer();
+            Models.User_Answer_Record_File obj = new Models.User_Answer_Record_File();
 
             dynamic jsonArrs = JArray.Parse(value);
 
@@ -70,24 +70,24 @@ namespace PTEWebService.Controllers
             int exam_id = jsonArr.exam_id;
             int question_id = jsonArr.question_id;
             int user_id = jsonArr.user_id;
-            string answer = jsonArr.answer;
-            int total_record_time = jsonArr.total_record_time;
-            string answer_submitted_date_time = jsonArr.answer_submitted_date_time;
+            string av_file_url = jsonArr.av_file_url;
+            int duration_of_file = jsonArr.duration_of_file;
+            string recorded_date_time = jsonArr.recorded_date_time;
             int created_by_id = jsonArr.created_by_id;
             //int index_id = jsonArr.index_id;
             //JSONArray ja_data = json.getJSONArray("data");
 
-            string output = obj.EditUserAnswer(id, exam_id, question_id, user_id, answer, total_record_time, answer_submitted_date_time, created_by_id);
+            string output = obj.EditUserAnswerRecordFile(id, exam_id, question_id, user_id, av_file_url, duration_of_file, recorded_date_time, created_by_id);
 
             return output;
         }
 
-        // DELETE: api/useranswer/5
+        // DELETE: api/useranswerrecordfile/5
         public string Delete(int id)
         {
-            Models.User_Answer obj = new Models.User_Answer();
+            Models.User_Answer_Record_File obj = new Models.User_Answer_Record_File();
 
-            string output = obj.DeleteUserAnswer(id);
+            string output = obj.DeleteUserAnswerRecordFile(id);
 
             return output;
         }
