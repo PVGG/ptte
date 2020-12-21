aad611c73416cd63e028a2b9eed1d617eea1e1a0
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-21 12:30:56 +0530

    Create New Apis

---
+++ PTEWebService/PTEWebService/Models/Role_Mst.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace PTEWebService.Models
+{
+    public class Role_Mst
+    {
+        public int index_id { get; set; }
+        public string role_name { get; set; }
+        public int user_id { get; set; }
+
+        private SqlConnection con;
+        private SqlCommand com;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+        }
+        #region Get All Role
+        public string GetAllRole()
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "SelectAll");
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Get Role from Index ID
+        public string GetRole(int id)
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "Select");
+                com.Parameters.AddWithValue("@index_id", user_id);
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Insert Role
+        public string AddRole(String name)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Insert");
+            com.Parameters.AddWithValue("@module_name", name);
+            com.Parameters.AddWithValue("@created_by", user_id);
+            com.Parameters.AddWithValue("@is_deleted", false);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+            {
+                return "Data Added Successfully";
+            }
+            else
+            {
+                return "Data Not Added";
+            }
+        }
+        #endregion
+        #region Update Role
+        public string EditRole(int index_id, string name)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Update");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@module_name", name);
+            com.Parameters.AddWithValue("@updated_date", DateTime.Now.ToShortDateString());
+            com.Parameters.AddWithValue("@updated_by", "UserId");
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Updated Successfully";
+            }
+            else
+            {
+                return "Data Not Updated";
+            }
+        }
+        #endregion
+        #region Delete Role
+        public string DeleteRole(int index_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Delete");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@is_deleted", true);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Deleted Successfully";
+            }
+            else
+            {
+                return "Data Not Deleted";
+            }
+        }
+        #endregion
+    }
+}
