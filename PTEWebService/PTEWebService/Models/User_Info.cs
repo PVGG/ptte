822fef616419f97e5ad80644338e4fe2b55c9aef
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-21 15:31:57 +0530

    Create New Api

---
+++ PTEWebService/PTEWebService/Models/User_Info.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace PTEWebService.Models
+{
+    public class User_Info
+    {
+        public int index_id { get; set; }
+        public string user_code { get; set; }
+        public string user_name { get; set; }
+        public string user_full_name { get; set; }
+        public string password { get; set; }
+        public int user_role_id { get; set; }
+        public string user_email { get; set; }
+        public string user_mobile_no { get; set; }
+        public string user_photo { get; set; }
+        public int is_approved { get; set; }
+        public int is_locked { get; set; }
+        public int user_failed_password_attempt_count { get; set; }
+        public int parent_id { get; set; }
+        public int customer_id { get; set; }
+        public string locked_reason { get; set; }
+        public int user_id { get; set; }
+
+
+        
+        private SqlConnection con;
+        private SqlCommand com;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+        }
+        #region Get All User Info
+        public string GetAllUserInfo()
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "SelectAll");
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Get User Info from Index ID
+        public string GetUserInfo(int id)
+        {
+            try
+            {
+                connection();
+                com = new SqlCommand("prManageModules", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Action", "Select");
+                com.Parameters.AddWithValue("@index_id", user_id);
+                com.Parameters.AddWithValue("@is_deleted", false);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+
+                if (i >= 1)
+                {
+                    return "Get Data Successfully";
+                }
+                else
+                {
+                    return "Get Data Not Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return null;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+        #region Insert User Info
+        public string AddUserInfo(string user_code, string user_name, string user_full_name, string password, int user_role_id, string user_email, string user_mobile_no, string user_photo, int is_approved, int is_locked, int user_failed_password_attempt_count, int parent_id, int customer_id, string locked_reason, int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Insert");
+            com.Parameters.AddWithValue("@module_name", area_name);
+            com.Parameters.AddWithValue("@created_by", created_by_id);
+            com.Parameters.AddWithValue("@is_deleted", false);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+            {
+                return "Data Added Successfully";
+            }
+            else
+            {
+                return "Data Not Added";
+            }
+        }
+        #endregion
+        #region Update User Info
+        public string EditUserInfo(int index_id,string user_code,string user_name,string user_full_name,string password,int user_role_id,string user_email,string user_mobile_no,string user_photo,int is_approved,int is_locked,int user_failed_password_attempt_count,int parent_id,int customer_id,string locked_reason,int user_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Update");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@module_name", area_name);
+            com.Parameters.AddWithValue("@updated_date", DateTime.Now.ToShortDateString());
+            com.Parameters.AddWithValue("@updated_by", "UserId");
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Updated Successfully";
+            }
+            else
+            {
+                return "Data Not Updated";
+            }
+        }
+        #endregion
+        #region Delete User Info
+        public string DeleteUserInfo(int index_id)
+        {
+            connection();
+            com = new SqlCommand("prManageModules", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Action", "Delete");
+            com.Parameters.AddWithValue("@index_id", index_id);
+            com.Parameters.AddWithValue("@is_deleted", true);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return "Data Deleted Successfully";
+            }
+            else
+            {
+                return "Data Not Deleted";
+            }
+        }
+        #endregion
+    }
+}
