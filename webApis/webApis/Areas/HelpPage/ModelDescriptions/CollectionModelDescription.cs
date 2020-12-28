fe49433202b789447cfac17704e46b686701ef1e
vishwajeetghungure23 <30863242+vishwajeetghungure23@users.noreply.github.com>
2020-12-28 16:13:40 +0530

    Remove Old Code

--- webApis/webApis/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace webApis.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
